0d6931052e4982f6ff4e2b15aa037d078810a249
Daniel Dogaru <danieldogaru7003@gmail.com>
2024-12-03 08:51:35 +0000

    Add the capability to extract a clause that is in a document between two headers

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,14 +1,89 @@
+// import fs from "fs";
+
 /* global Word console */
 
+// To avoid duplicates we need to have the range of text (ideally the paragraph) where
+// this edit needs to be. For now hardcoding it.
+
+const uniqueStyles = [];
+
 export async function insertText(text) {
-  // Write text to the document.
-  try {
-    await Word.run(async (context) => {
-      let body = context.document.body;
-      body.insertParagraph(text, Word.InsertLocation.end);
+  // const sample_response = JSON.parse(fs.readFileSync("./src/taskpane/sample_response_1.json", "utf8"));
+
+  await Word.run(async (context) => {
+    let clauseRangeResult = await getClauseRange(context, "Delivery of Battery Packages to Site");
+    if (clauseRangeResult.foundRange) {
+      clauseRangeResult.range.load("text");
       await context.sync();
-    });
-  } catch (error) {
-    console.log("Error: " + error);
+      console.log("Clause range text: " + clauseRangeResult.range.text);
+    }
+    console.log("End of the execution");
+  });
+}
+
+async function getClauseRange(context, clauseHeaderText) {
+  const paragraphs = context.document.body.paragraphs;
+  paragraphs.load("items/style/name,items/text");
+  await context.sync();
+
+  let startRange = null;
+  let endRange = null;
+
+  for (let i = 0; i < paragraphs.items.length; i++) {
+    const paragraph = paragraphs.items[i];
+
+    // ============ Debug code ====================
+    if (!uniqueStyles.includes(paragraph.style)) {
+      uniqueStyles.push(paragraph.style);
+    }
+
+    if (paragraph.text.includes(clauseHeaderText) && paragraph.style === "heading 1") {
+      startRange = paragraph.getRange();
+      continue;
+    }
+
+    if (startRange && paragraph.style === "heading 1") {
+      endRange = paragraph.getRange();
+      break;
+    }
+  }
+
+  console.log("Is this still executing");
+
+  // ============ Debug code - print styles ====================
+  // for (var style in uniqueStyles) {
+  //   console.log("Paragraph style:" + uniqueStyles[style]);
+  // }
+  // for (let i = 0; i < paragraphs.items.length; i++) {
+  //   const paragraph = paragraphs.items[i];
+  //   if (paragraph.style === "heading 1") {
+  //     console.log("Paragraph with style 'heading 1': " + paragraph.text);
+  //   }
+  // }
+  let result = {
+    foundRange: false,
+    range: null,
+  };
+
+  if (startRange && endRange) {
+    // console.log("Found range text: " + startRange.text + " " + endRange.text);
+    try {
+      // Expand startRange to include endRange
+      result.foundRange = true;
+      result.range = startRange.expandTo(endRange);
+      return result;
+    } catch (error) {
+      console.error("Error during context.sync():", error);
+    }
+  } else {
+    console.log("startRange or endRange is not defined");
+    if (!startRange) {
+      console.log("startRange is not defined");
+    }
+    if (!endRange) {
+      console.log("endRange is not defined");
+    }
+
+    return Promise.resolve(result);
   }
 }
